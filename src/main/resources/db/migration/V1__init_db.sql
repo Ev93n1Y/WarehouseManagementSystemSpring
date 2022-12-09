862a25505e321af2a113801cda2319a37130f3a6
Yevhen <96913622+Ev93n1Y@users.noreply.github.com>
2022-12-09 17:10:07 +0200

    add migration

---
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -0,0 +1,36 @@
+CREATE TABLE producers
+(
+    id   UUID         PRIMARY KEY ,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE products
+(
+    id          UUID         PRIMARY KEY ,
+    name        VARCHAR(100) NOT NULL,
+    price       DECIMAL(10, 2),
+    producer_id UUID REFERENCES producers (id) ON DELETE CASCADE
+);
+
+CREATE TABLE roles
+(
+    id   UUID         PRIMARY KEY ,
+    role VARCHAR(100) NOT NULL,
+	CONSTRAINT uc_roles_role UNIQUE (role)
+);
+
+CREATE TABLE users
+(
+    id        UUID         PRIMARY KEY ,
+    email     VARCHAR(100) NOT NULL UNIQUE,
+    password  VARCHAR(100),
+    first_name VARCHAR(100),
+    last_name  VARCHAR(100)
+);
+
+CREATE TABLE user_roles
+(
+    role_id UUID NOT NULL REFERENCES roles (id),
+    user_id UUID NOT NULL REFERENCES users (id),
+	PRIMARY KEY (role_id, user_id)
+);
